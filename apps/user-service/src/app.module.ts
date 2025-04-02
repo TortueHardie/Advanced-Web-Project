76c651511a70afce0f11d86a1d963e05f3c8d39b
HugoMartinTetraDev <code --wait>
2025-04-02 16:43:56 +0200

    Suppression du contrôleur et du service de l'application dans le module utilisateur, simplifiant ainsi la structure du code. Mise à jour des contrôleurs et des fournisseurs pour ne conserver que ceux nécessaires au service utilisateur.

--- apps/user-service/src/app.module.ts
+++ apps/user-service/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
 import { UserRepository } from './repositories/user.repository';
@@ -8,7 +6,7 @@ import { PrismaService } from '../../../packages/prisma/src/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService, UserRepository, PrismaService],
+  controllers: [UserController],
+  providers: [UserService, UserRepository, PrismaService],
 })
 export class AppModule {}
