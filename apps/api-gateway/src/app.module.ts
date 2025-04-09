3288b314594226a8ae612ab4ee9545e2a1f556aa
HugoMartinTetraDev <hmartin@tetralink.fr>
2025-04-09 14:03:45 +0200

    Revert "Add logs service"

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_PIPE } from '@nestjs/core';
@@ -8,7 +8,6 @@ import { AppService } from './app.service';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UserController } from './controllers/user.controller';
-import { LoggerMiddleware } from './middleware/logger.middleware';
 import { OrderController } from './controllers/order.controller';
 import { GlobalExceptionFilter } from '../../../packages/common/src/error-handling/http-exception.filter';
 
@@ -33,10 +32,4 @@ import { GlobalExceptionFilter } from '../../../packages/common/src/error-handli
     }
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes('*'); // Applique le middleware à toutes les routes
-  }
-}
+export class AppModule {}
